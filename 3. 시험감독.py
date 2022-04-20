284b5cb4339a9f5906a50665b0ebce0193b81a77
sinyo2-ssu <niybr333@naver.com>
2022-04-20 16:00:04 +0900

    220420

---
+++ 3. 시험감독.py
@@ -0,0 +1,17 @@
+N = int(input())
+arr = list(map(int, input().split()))
+Master, Sub = map(int, input().split())
+
+cnt = len(arr)
+for i in range(len(arr)):
+    arr[i] -= Master
+    if arr[i] <= 0:
+        continue
+    else :
+        if arr[i] % Sub == 0:
+            cnt += int(arr[i] / Sub)
+        else :
+            cnt += int((arr[i] / Sub)) + 1
+
+print(cnt)
+
