82fe1d2ae275c5d20fe863908d24221948c85948
sinyo2-ssu <mrazsin@gmail.com>
2022-04-26 19:18:01 +0900

    220426

--- 6. 로봇 청소기.py
+++ 6. 로봇 청소기.py
@@ -135,4 +135,3 @@ for i in range(len(s)):
 #     print(s[i])
 print(cnt)
 
-
---
+++ 7. 테트로미노.py
@@ -0,0 +1,44 @@
+tetrominos = []
+tet1_1 = [[1,1,1,1]]
+tetrominos.append(tet1_1)
+tet1_2 = [[1],[1],[1],[1]]
+tetrominos.append(tet1_2)
+tet2_1 = [[1,1],[1,1]]
+tetrominos.append(tet2_1)
+tet3_1 = [[1,0],[1,0],[1,1]]
+tetrominos.append(tet3_1)
+tet3_2 = [[0,0,1],[1,1,1]]
+tetrominos.append(tet3_2)
+tet3_3 = [[1,1],[0,1],[0,1]]
+tetrominos.append(tet3_3)
+tet3_4 = [[1,1,1],[1,0,0]]
+tetrominos.append(tet3_4)
+tet4_1 = [[1,0],[1,1],[0,1]]
+tetrominos.append(tet4_1)
+tet4_2 = [[0,1,1],[1,1,0]]
+tetrominos.append(tet4_2)
+tet5_1 = [[1,1,1],[0,1,0]]
+tetrominos.append(tet5_1)
+tet5_2 = [[0,1],[1,1],[0,1]]
+tetrominos.append(tet5_2)
+tet5_3 = [[0,1,0],[1,1,1]]
+tetrominos.append(tet5_3)
+tet5_4 = [[1,0],[1,1],[1,0]]
+tetrominos.append(tet5_4)
+
+N, M = map(int, input().split())
+
+s = []
+for i in range(N):
+    s.append(list(map(int,input().split())))
+
+print(tetrominos)
+for tetro_idx in range(len(tetrominos)):
+    for i in range(N-len(tetrominos[tetro_idx])+1):
+        for j in range(M-len(tetrominos[tetro_idx][0])+1):
+            print(tetrominos[tetro_idx])
+        print(" ")
+
+
+
+
