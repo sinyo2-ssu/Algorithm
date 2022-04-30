7e80b6f05c0314a2e34805e893ff08d4b3aaac05
sinyo2-ssu <mrazsin@gmail.com>
2022-04-30 17:25:30 +0900

    220430

--- _11. 탈주범 검거.py
+++ _11. 탈주범 검거.py
@@ -1,64 +1,39 @@
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
-
-# 표준 입력 예제
-'''
-a = int(input())                        정수형 변수 1개 입력 받는 예제
-b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제
-d = float(input())                      실수형 변수 1개 입력 받는 예제
-e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
-h = input()                             문자열 변수 1개 입력 받는 예제
-'''
-
-# 표준 출력 예제
-'''
-a, b = 6, 3
-c, d, e = 1.0, 2.5, 3.4
-f = "ABC"
-print(a)                                정수형 변수 1개 출력하는 예제
-print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
-print(c, d, e)                          실수형 변수 3개 출력하는 예제
-print(f)                                문자열 1개 출력하는 예제
-'''
-
-import sys
-
-
-'''
-      아래의 구문은 input.txt 를 read only 형식으로 연 후,
-      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-
-      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
-
-      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
-#sys.stdin = open("input.txt", "r")
-
-def bfs(arr, type,x,y):
-    if type == 1:
-        if arr[x-1][y] == 1 or arr[x-1][y] == 2 or arr[x-1][y] == 5 or arr[x-1][y] == 6:
-            visited[x-1][y] = True
-
-
+def bfs(N, M):
+    way = [(0, -1), (-1, 0), (0, 1), (1, 0)]  # 좌, 상, 우, 하
+    pipe = [[], [0, 1, 2, 3], [1, 3], [0, 2], [1, 2], [3, 2], [3, 0], [1, 0]]
+    while q:
+        x, y = q.pop(0)
+        sub = pipe[fig[x][y]]
+        if sub == []:
+            continue
+        for i in sub:
+            di, dj = way[i]
+            nx, ny = x + di, y + dj
+            if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == 0 and fig[nx][ny] != 0:
+                next = pipe[fig[nx][ny]]
+                for j in next:
+                    mx, my = nx + way[j][0], ny + way[j][1]
+                    if mx == x and my == y:
+                        q.append((nx, ny))
+                        visited[nx][ny] = visited[x][y] + 1
 
 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-
-# 1: + 2: ㅣ 3: ㅡ 4: ㄴ 5:
-
-for test_case in range(1, T + 1):
+for tc in range(T):
+    # N, M : 지도의 세로, 가로
+    # R, C : 맨홀 뚜껑의 세로, 가로 (출발점)
+    # L : 탈출 후 소요된 시간
     N, M, R, C, L = map(int, input().split())
-    visited = [[False for col in range(M)] for row in range(N)]
-    s = []
+    fig = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+    visited[R][C] = 1  # 맨홀뚜껑 위치
+    q = [(R, C)]
+    bfs(N, M)
+
+    cnt = 0
     for i in range(N):
-        s.append(list(map(int, input().split())))
+        for j in range(M):
+            if 0 < visited[i][j] <= L:
+                cnt += 1
 
-    # for i in range(N):
-    #     print(visited[i])
-    # print("")
-
-
+    print(f'#{tc+1} {cnt}')
